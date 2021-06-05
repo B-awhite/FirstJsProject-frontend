3010a90a0586525def0290421bc9fad4ed8755fd
bria_white <bria.awhite@gmail.com>
2021-06-04 23:24:19 -0400

    Delete function & event listeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     addCreateForm();
     fetchBeaches();
+    listenDelete();
 })
 
 
@@ -10,9 +11,7 @@ function fetchBeaches() {
     fetch("http://localhost:3000/api/v1/beaches")
     .then(r => r.json())
     .then(data => {
-       data.forEach(function(beach){
-           beachesContainer.innerHTML += `<li>${beach.name}</li>`
-       })
+       data.forEach(addBeach)
     })
     .catch(err => console.warn(err))
 }
@@ -30,15 +29,57 @@ function addCreateForm() {
 function addBeach(event) {
     event.preventDefault()
     const nameInput = event.target.children[0]
-    
-    fetch("http://localhost:3000/api/vi/beaches", {
+    const countryInput = event.target.children[0]
+
+    fetch("http://localhost:3000/api/v1/beaches", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json"
         },
         body: JSON.stringify({
-            name: nameInput.value
+            name: nameInput.value,
+            country: countryInput.value
         })
     })
+    .then(response => response.json())
+    .then(info => {
+        console.log("Created not saved", info)
+        if (info.status === 201){
+            addBeach(info.beach)
+        } else {
+            alert(info.errors)
+        }
+    })
+    .catch(err => console.error("Not created", err))
+}
+
+function addBeach(beach) {
+    const beachesContainer = document.getElementById("beaches-container");
+    beachesContainer.innerHTML += `<li data-id=${beach.id}>${beach.name} ${beach.country} <button data-action='delete'>X</button></li>`
+}
+
+function listenDelete() {
+    const beachesContainer = document.getElementById("beaches-container");
+
+    beachesContainer.addEventListener("click", deleteBeach)
+}
+
+function deleteBeach(event) {
+    if (event.target.dataset.action === "delete"){
+        const li = event.target.parentElement
+
+        fetch(`http://localhost:3000/api/v1/beaches/${li.dataset.id}`, {
+            method: "DELETE"
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.message === "Beach deleted"){
+                li.remove()
+            } else {
+                alert(data.message)
+            }
+        })
+        .catch(err => console.error(err))
+    } 
 }
