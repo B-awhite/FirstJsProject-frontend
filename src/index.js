00c446df439927ae20f4d6cc2bb841135a49262b
bria_white <bria.awhite@gmail.com>
2021-06-06 21:18:21 -0400

    edit/update functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// let editMode = false
+let editMode = false
 
 document.addEventListener("DOMContentLoaded", () => {
     addCreateForm();
     fetchBeaches();
     // listenEdit();
-    listenDelete();
+    listenEditDelete();
 })
 
 
@@ -22,7 +22,7 @@ function fetchBeaches() {
 function addCreateForm() {
     const formContainer = document.getElementById("form-container");
     const form = document.createElement("form")
-    form.innerHTML = `<input placeholder='Name' type='text'/><input placeholder='Country' type='text'/><input id="beach-submit" value="Add Beach" type='submit'/>`
+    form.innerHTML = `<input id="name-input" placeholder='Name' type='text'/><input id="country-input" placeholder='Country' type='text'/><input id="beach-submit" value="Add Beach" type='submit'/>`
     formContainer.append(form)
 
     form.addEventListener("submit", addBeach)
@@ -32,33 +32,59 @@ function addBeach(event) {
     event.preventDefault()
     const nameInput = event.target.children[0]
     const countryInput = event.target.children[0]
-
-    fetch("http://localhost:3000/api/v1/beaches", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        },
-        body: JSON.stringify({
-            name: nameInput.value,
-            country: countryInput.value
+    if (editMode){
+        fetch(`http://localhost:3000/api/v1/beaches/${editMode.dataset.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: nameInput.value,
+                country: countryInput.value
+            })
         })
-    })
-    .then(response => response.json())
-    .then(info => {
-        console.log("Created not saved", info)
-        if (info.status === 201){
-            newBeach(info.beach)
-        } else {
-            alert(info.errors)
-        }
-    })
-    .catch(err => console.error("Not created", err))
+        .then(resp => resp.json())
+        .then(info => {
+            if (info.status === 200) {
+              editMode.children[0].innerText = info.beach.name
+              editMode = false
+              document.getElementById('beach-submit').value = "Create Beach"
+              nameInput.value = ""
+            } else {
+                alert(info.errors)
+            }
+        })
+        .catch(err => console.error(err))
+    } else {
+        fetch("http://localhost:3000/api/v1/beaches", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+           },
+           body: JSON.stringify({
+              name: nameInput.value,
+              country: countryInput.value
+           })
+       })
+      .then(response => response.json())
+      .then(info => {
+          console.log("Created not saved", info)
+          if (info.status === 201){
+              newBeach(info.beach)
+          } else {
+              alert(info.errors)
+          }
+          nameInput.value = ""
+      })
+      .catch(err => console.error("Not created", err))
+    }
 }
 
 function newBeach(beach) {
     const beachesContainer = document.getElementById("beaches-container");
-    beachesContainer.innerHTML += `<li data-id=${beach.id}>${beach.name} ${beach.country} <button data-action='edit'>EDIT</button> <button data-action='delete'>X</button></li>`
+    beachesContainer.innerHTML += `<li data-id=${beach.id}><span>${beach.name}, ${beach.country}</span> <button data-action='edit'>Edit</button> <button data-action='delete'>X</button></li><br>`
 }
 
 // function listenEdit() {
@@ -76,14 +102,14 @@ function newBeach(beach) {
 //     }
 // }
 
-function listenDelete() {
+function listenEditDelete() {
     const beachesContainer = document.getElementById("beaches-container");
-    beachesContainer.addEventListener("click", deleteBeach)
+    beachesContainer.addEventListener("click", editDeleteBeach)
 }
 
-function deleteBeach(event) {
+function editDeleteBeach(event) {
+    const li = event.target.parentElement
     if (event.target.dataset.action === "delete"){
-        const li = event.target.parentElement
 
         fetch(`http://localhost:3000/api/v1/beaches/${li.dataset.id}`, {
             method: "DELETE"
@@ -97,7 +123,12 @@ function deleteBeach(event) {
             }
         })
         .catch(err => console.error(err))
-    } 
+    } else if (event.target.dataset.action === "edit"){
+        editMode = li 
+        document.getElementById('beach-submit').value = "Update Beach"
+        document.getElementById('name-input').value = li.children[0].innerText
+        document.getElementById('country-input').value = li.children[0].innerText
+    }
 }
 
 // function deleteBeach(event) {
